9cfae41652e8dfb99d75a9c714c9541524c6dbdd
temporizer <temporizer@gmail.com>
2014-04-21 20:19:54 -0700

    Added index and framework

---
+++ index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Truth About Tattoos - T.A.T.</title>
+	<link rel="stylesheet" href="css/style.css" media="screen">
+	<link href='http://fonts.googleapis.com/css?family=Fredericka+the+Great|Metal+Mania' rel='stylesheet' type='text/css'>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+</head>
+<body>
+	<section id="wrapper">
+		<header>
+			<div class="logo">
+				<p>T.A.T.</p>
+			</div>
+			<div class="title">
+				<p>Truth About Tattoos<p>
+			</div>
+
+			<nav>
+				
+			</nav>
+		</header> <!-- end header -->
+
+		<section id="sidebar">
+			
+		</section> <!-- end sidebar -->
+		
+		<section id="content">
+			<h1>This is a header</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum pariatur repudiandae vitae eligendi saepe illo nam illum. Distinctio, maxime inventore sint saepe facilis perspiciatis in repellendus maiores sequi. Saepe, optio, recusandae expedita magni cupiditate quis est dolorem natus unde tempore nemo minus in maxime ex incidunt eum consequatur tenetur atque neque sequi provident exercitationem velit ad iusto animi temporibus earum corporis consectetur reprehenderit ducimus officiis quos voluptatem culpa? Explicabo, repudiandae, unde magnam saepe dolores officia beatae animi dolorum asperiores tempora facere aut dicta modi quae sapiente non consectetur doloribus similique ex debitis neque obcaecati atque ea soluta quo assumenda corporis.</p>
+			
+		</section> <!-- end content -->
+		
+	</section> <!-- end wrapper -->
+
+
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+	<script src="js/script.js"></script>
+</body>
+</html>
