ff8a312818ced1959caf15cdc3017eac5114ab18
temporizer <temporizer@gmail.com>
2014-04-21 20:23:49 -0700

    Changed index.php

--- index.php
+++ index.php
@@ -19,20 +19,20 @@
 			</div>
 
 			<nav>
-				
+
 			</nav>
 		</header> <!-- end header -->
 
 		<section id="sidebar">
-			
+
 		</section> <!-- end sidebar -->
-		
+
 		<section id="content">
 			<h1>This is a header</h1>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum pariatur repudiandae vitae eligendi saepe illo nam illum. Distinctio, maxime inventore sint saepe facilis perspiciatis in repellendus maiores sequi. Saepe, optio, recusandae expedita magni cupiditate quis est dolorem natus unde tempore nemo minus in maxime ex incidunt eum consequatur tenetur atque neque sequi provident exercitationem velit ad iusto animi temporibus earum corporis consectetur reprehenderit ducimus officiis quos voluptatem culpa? Explicabo, repudiandae, unde magnam saepe dolores officia beatae animi dolorum asperiores tempora facere aut dicta modi quae sapiente non consectetur doloribus similique ex debitis neque obcaecati atque ea soluta quo assumenda corporis.</p>
-			
+			<p>orem ipsum dolor sit amet, consectetur adipisicing elit. Cum pariatur repudiandae vitae eligendi saepe illo nam illum. Distinctio, maxime inventore sint saepe facilis perspiciatis in repellendus maiores sequi. Saepe, optio, recusandae expedita magni cupiditate quis est dolorem natus unde tempore nemo minus in maxime ex incidunt eum consequatur tenetur atque neque sequi provident exercitationem velit ad iusto animi temporibus earum corporis consectetur reprehenderit ducimus officiis quos voluptatem culpa? Explicabo, repudiandae, unde magnam saepe dolores officia beatae animi dolorum asperiores tempora facere aut dicta modi quae sapiente non consectetur doloribus similique ex debitis neque obcaecati atque ea soluta quo assumenda corporis.</p>
+
 		</section> <!-- end content -->
-		
+
 	</section> <!-- end wrapper -->
 
 
